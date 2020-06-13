9933aae28bd7d7702d2899fc65c23becfb0d8bfd
prashant-tiwari <prashant-tiwari@LPT1345.newgen.co.in>
2020-06-13 19:05:50 +0530

    Ok

--- Calculator/src/com/calculator/Calculate.java
+++ Calculator/src/com/calculator/Calculate.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 
 public class Calculate {
 
+	private static Scanner reader;
+
 	public static void main(String[] args) {
 
 
-        Scanner reader = new Scanner(System.in);
+        reader = new Scanner(System.in);
         System.out.print("Enter two numbers: ");
 
         // nextDouble() reads the next double from the keyboard
